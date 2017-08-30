593ab554816b3489220413e20fe85577d7d8ff0d
Scott Linderman <scott.linderman@gmail.com>
2017-08-30 15:56:02 -0400

    tweaks

--- two_population_demo.py
+++ two_population_demo.py
@@ -16,7 +16,8 @@ np.random.seed(seed)
 N1 = 25         # Number of neurons in pop. 1
 N2 = 25         # Number of neurons in pop. 2
 K = 20          # Number of states
-T = 1000        # Number of time bins to simulate per trial
+T_train = 1000  # Number of time bins to simulate per trial
+T_test = 100    # Number of time bins to simulate per trial
 N_iter = 500    # Number of iterations of Gibbs sampling
 
 # Simulate from an HMM with a known transition matrix
@@ -25,18 +26,19 @@ true_A /= true_A.sum(axis=1)[:,None]
 true_hmm = pyhsmm_spiketrains.models.PoissonHMM(N=N1+N2, K=K, trans_matrix=true_A)
 
 # Generate training data and testing data
-S_train, _ = true_hmm.generate(T, keep=True)
+S_train, _ = true_hmm.generate(T_train, keep=True)
 S1_train, S2_train = S_train[:, :N1].copy('C'), S_train[N1:].copy('C')
-S_test, _ = true_hmm.generate(T, keep=True)
+S_test, _ = true_hmm.generate(T_test, keep=True)
 S1_test, S2_test = S_test[:, :N1].copy('C'), S_test[N1:].copy('C')
 
 # Mask off the activity from population 2
-mask1 = np.hstack([np.ones((T, N1), dtype=bool), np.zeros((T, N2), dtype=bool)])
+M1_train = np.hstack([np.ones((T_train, N1), dtype=bool), np.zeros((T_train, N2), dtype=bool)])
+M1_test = np.hstack([np.ones((T_test, N1), dtype=bool), np.zeros((T_test, N2), dtype=bool)])
 
 # Compute true likelihoods
-ll_S1tr_true = true_hmm.log_likelihood(S_train, mask=mask1)
-ll_S1te_true = true_hmm.log_likelihood(S_test, mask=mask1)
-ll_S2te_given_S1te_true = true_hmm.predictive_log_likelihood(S_test, mask=mask1)
+ll_S1tr_true = true_hmm.log_likelihood(S_train, mask=M1_train)
+ll_S1te_true = true_hmm.log_likelihood(S_test, mask=M1_test)
+ll_S2te_given_S1te_true = true_hmm.predictive_log_likelihood(S_test, mask=M1_test)
 
 print("Fitting naive model on population 1 alone")
 naive_hmm = pyhsmm_spiketrains.models.PoissonHDPHMM(N=N1+N2, K_max=100)
@@ -55,15 +57,15 @@ for itr in progprint_xrange(N_iter):
     aug_hmm.resample_model()
 
 # Evaluate the likelihood of population 1 under the full model
-ll_S1tr_aug = aug_hmm.log_likelihood(S_train, mask=mask1)
-ll_S1te_aug = aug_hmm.log_likelihood(S_test, mask=mask1)
+ll_S1tr_aug = aug_hmm.log_likelihood(S_train, mask=M1_train)
+ll_S1te_aug = aug_hmm.log_likelihood(S_test, mask=M1_test)
 
 # We can also compute the likelihood of the entire dataset
 ll_Str = aug_hmm.log_likelihood(S_train)
 ll_Ste = aug_hmm.log_likelihood(S_test)
 
 # And we can compute the predictive likelihood using masks
-ll_S2te_given_S1te_aug = aug_hmm.predictive_log_likelihood(S_test, mask=mask1)
+ll_S2te_given_S1te_aug = aug_hmm.predictive_log_likelihood(S_test, mask=M1_test)
 
 
 print("Let theta_naive denote the parameters learned by naively "
